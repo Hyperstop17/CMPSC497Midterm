ba1b8f2a3e9e36880263b2180644a0ad6d5f3603
Hyperstop17 <damientkula@gmail.com>
2025-02-28 05:08:21 -0500

    Started work on the dataset. File succesfully opens and splitting works. Have to do CNN part next.

--- CMPSC497 Midterm Project.py
+++ CMPSC497 Midterm Project.py
@@ -1,2 +1,20 @@
 #CMPSC497 Midterm Project
 #Damien Kula and Hamid Shah
+
+#Opening following dataset: https://www.kaggle.com/datasets/sameedatif/tone-analysis on Kaggle
+dataset = open("C:\\Users\\damie\\Documents\\Python\\Datasets\\tone_dataset_shuffled.txt", encoding="utf8")
+
+training_data = []
+training_labels = []
+
+#the data has 3356 sentences so we use ~70% for training
+for i in range(2410):
+    sentence = dataset.readline()
+    values = sentence.split(" || ")
+    label = values[1][:-1]
+    training_data.append(values[0])
+    training_labels.append(label)
+
+print(training_data)
+print(training_labels)
+dataset.close()
